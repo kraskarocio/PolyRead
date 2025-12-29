2226a8fa5b397700119310dd6ad125153d9cecbf
kraskarocio <rkraska@fi.uba.ar>
2025-12-28 23:42:18 -0300

    initial config

---
+++ backend/src/main/java/com/polyread/backend/PolyreadApplication.java
@@ -0,0 +1,13 @@
+package com.polyread.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyreadApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/polyread/backend/PolyreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polyread.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
